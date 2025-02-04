6cfd405296c5978cb7f1b38a27db8ca0ac0daa38
llarroquette27 <lukalar@umich.edu>
2025-02-04 12:05:44 -0500

    got first page working

---
+++ insta485/api/index.py
@@ -0,0 +1,16 @@
+"""REST API for / route"""
+import flask
+import insta485
+
+@insta485.app.route('/api/v1/')
+def get_index():
+    """Return index"""
+
+    context = {
+        "comments": "/api/v1/comments/",
+        "likes": "/api/v1/likes/",
+        "posts": "/api/v1/posts/",
+        "url": "/api/v1/",
+    }    
+
+    return flask.jsonify(**context)
