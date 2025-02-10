263801cd3da5ab6b1a91f85c7176beec2e259d01
llarroquette27 <lukalar@umich.edu>
2025-02-10 12:36:08 -0500

    Implemented content of main page with react, including like/dislike

--- insta485/js/post.jsx
+++ insta485/js/post.jsx
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
 
 // The parameter of this function is an object with a string called url inside it.
 // url is a prop for the Post component.
 export default function Post({ url }) {
   /* Display image and post owner of a single post */
 
+  const [postInfo, setPostInfo] = useState(null);
   const [imgUrl, setImgUrl] = useState("");
   const [owner, setOwner] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const [time, setTime] = useState("");
+  const [likes, setLikes] = useState(0);
+  const [comments, setComments] = useState([]);
+  const [isLiked, setIsLiked] = useState(false);
 
   useEffect(() => {
     // Declare a boolean flag that we can use to cancel the API request.
@@ -19,11 +31,18 @@ export default function Post({ url }) {
         return response.json();
       })
       .then((data) => {
+        console.log("DATA: ", data);
         // If ignoreStaleRequest was set to true, we want to ignore the results of the
         // the request. Otherwise, update the state to trigger a new render.
         if (!ignoreStaleRequest) {
+          setPostInfo(data);
           setImgUrl(data.imgUrl);
           setOwner(data.owner);
+          setProfilePic(data.ownerImgUrl);
+          setTime(data.created);
+          setLikes(data.likes.numLikes);
+          setComments(data.comments);
+          setIsLiked(data.likes.lognameLikesThis);
         }
       })
       .catch((error) => console.log(error));
@@ -36,11 +55,87 @@ export default function Post({ url }) {
     };
   }, [url]);
 
+  const handleLike = async () => {
+    setIsLiked(true);
+    setLikes(likes + 1);
+
+    // Post to rest API
+    try {
+      const response = await fetch(`/api/v1/likes/?postid=${postInfo.postid}`, {method: "POST"});
+      const data = await response.json();
+      console.log("SUCCESS: ", data);
+    } catch (error) {
+      console.error('Error: ', error);
+    }
+  };
+
+  const handleDislike = async () => {
+    setIsLiked(false);
+    setLikes(likes - 1);
+
+    // Post to rest API
+    try {
+      const response = await fetch(`/api/v1/likes/?postid=${postInfo.postid}`, {method: "DELETE"});
+      const data = await response.json();
+      console.log("SUCCESS: ", data);
+    } catch (error) {
+      console.error('Error: ', error);
+    }
+  };
+
   // Render post image and post owner
+  // ADD: Comment owner link, like button, comment button, humanized timestamp
   return (
     <div className="post">
-      <img src={imgUrl} alt="post_image" />
       <p>{owner}</p>
+      <img src={profilePic} alt="profile_pic" />
+      <p>{time}</p>
+      <img src={imgUrl} alt="post_image" />
+      {likes === 1 ? (
+        <div>{likes} like</div>
+      ) : (
+        <div>{likes} likes</div>
+      )}
+      {isLiked ? (
+        <button 
+          data-testid="like-unlike-button"
+          onClick={() => handleDislike()}
+        >
+          unlike
+        </button>
+        // <form action="/likes/" method="post" enctype="multipart/form-data">
+        //   <input type="hidden" name="operation" value="unlike" />
+        //   <input type="hidden" name="postid" value="{{ post.postid }}" />
+        //   <input type="submit" name="unlike" value="unlike" />
+        // </form>
+      ) : (
+        <button 
+          data-testid="like-unlike-button"
+          onClick={() => handleLike()}
+        >
+          like
+        </button>
+        // <form action="/likes/" method="post" enctype="multipart/form-data">
+        //   <input type="hidden" name="operation" value="like" />
+        //   <input type="hidden" name="postid" value="{{ post.postid }}" />
+        //   <input type="submit" name="like" value="like" />
+        // </form>
+      )}
+      {comments.map((c, index) => {
+        return (
+          <div>
+            <a href={c.ownerShowUrl}>{c.owner}</a>
+            <span>{c.text}</span>
+          </div>
+        )
+      })}
+      <button></button>
+      {/* <form action="/comments/" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="operation" value="create" />
+        <input type="hidden" name="postid" value="{{post.postid}}" />
+        <input type="text" name="text" required />
+        <input type="submit" name="comment" value="comment" />
+      </form> */}
     </div>
   );
 }
